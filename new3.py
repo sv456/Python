4721948ff2b24a621a26477c4ab737620eb1a500
Sandesh <vishwanathsandesh@gmail.com>
2016-06-06 19:39:44 +0530

    new3.py modified

--- new3.py
+++ new3.py
@@ -1,3 +1,29 @@
-import random
-i=[1,2,3]
-print random.choice(i)
+total = 0
+cnt = 0
+avg = 0
+
+def calculate(d):
+    global total
+    total += d
+    global cnt
+    cnt+=1
+    global avg
+    avg=float(total)/cnt
+
+
+def call():
+    print 'total:', total, 'count:', cnt, 'average:', avg
+
+
+while True:
+    data = raw_input('enter the number')
+    if data=='done':
+        call()
+        break
+    else:
+        try:
+            data=int(data)
+            calculate(data)
+        except:
+             print "Invalid input"
+             continue
