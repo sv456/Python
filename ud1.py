ed50adbe9d0465731cc9ba032b8d2983182e371c
Sandesh <vishwanathsandesh@gmail.com>
2016-06-13 23:21:51 +0530

    udacity program modified

--- ud1.py
+++ ud1.py
@@ -1,8 +1,9 @@
 import webbrowser
 import time
 n=1
-print "this program started on ",time.ctime()
+
 while n<=3:
     time.sleep(6)
+    print "this program started on ",time.ctime()
     webbrowser.open("https://www.youtube.com/watch?v=Jx2yQejrrUE")
     n+=1
