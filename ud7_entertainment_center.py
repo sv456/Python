64d2dcfe9ea9ddb800054cfb833f95d49d4d2012
Sandesh <vishwanathsandesh@gmail.com>
2016-06-21 18:56:12 +0530

    movie trailer project

---
+++ ud7_entertainment_center.py
@@ -0,0 +1,11 @@
+import ud7_media
+
+toy_story=ud7_media.Movie("Toy Story","Story of a boy and his toys","http://a.dilcdn.com/bl/wp-content/uploads/sites/8/2013/02/toy_story_wallpaper_by_artifypics-d5gss19.jpg","https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print toy_story.storyline
+
+avatar=ud7_media.Movie("Avatar","Story about aliens","https://www.google.com.sg/search?q=avatar+poster&tbm=isch&imgil=K44x3Ulq45exrM%253A%253BBR_9mF6APN6b2M%253Bhttps%25253A%25252F%25252Fwww.movieposter.com%25252Fq%25252Favatar_posters.html&source=iu&pf=m&fir=K44x3Ulq45exrM%253A%252CBR_9mF6APN6b2M%252C_&usg=__5pLH2he8ChqYQdrR44Y8OkWRmUk%3D&biw=1920&bih=955&ved=0ahUKEwjzscuvj7nNAhXJt48KHR-BDuMQyjcIQw&ei=9zJpV7PaJcnvvgSfgrqYDg#imgrc=K44x3Ulq45exrM%3A","https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+print avatar.storyline
+
+avatar.show_trailer()
