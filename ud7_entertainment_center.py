f87ef7bfdcc4387166129e60f431e0ee92517586
Sandesh <vishwanathsandesh@gmail.com>
2016-06-22 21:55:26 +0530

    new file for movies project

--- ud7_entertainment_center.py
+++ ud7_entertainment_center.py
@@ -1,11 +1,17 @@
 import ud7_media
+import fresh_tomatoes
 
 toy_story=ud7_media.Movie("Toy Story","Story of a boy and his toys","http://a.dilcdn.com/bl/wp-content/uploads/sites/8/2013/02/toy_story_wallpaper_by_artifypics-d5gss19.jpg","https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print toy_story.storyline
+#print toy_story.storyline
 
-avatar=ud7_media.Movie("Avatar","Story about aliens","https://www.google.com.sg/search?q=avatar+poster&tbm=isch&imgil=K44x3Ulq45exrM%253A%253BBR_9mF6APN6b2M%253Bhttps%25253A%25252F%25252Fwww.movieposter.com%25252Fq%25252Favatar_posters.html&source=iu&pf=m&fir=K44x3Ulq45exrM%253A%252CBR_9mF6APN6b2M%252C_&usg=__5pLH2he8ChqYQdrR44Y8OkWRmUk%3D&biw=1920&bih=955&ved=0ahUKEwjzscuvj7nNAhXJt48KHR-BDuMQyjcIQw&ei=9zJpV7PaJcnvvgSfgrqYDg#imgrc=K44x3Ulq45exrM%3A","https://www.youtube.com/watch?v=5PSNL1qE6VY")
+avatar=ud7_media.Movie("Avatar","Story about aliens","https://www.movieposter.com/posters/archive/main/98/MPW-49246","https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-print avatar.storyline
+#print avatar.storyline
 
-avatar.show_trailer()
+#avatar.show_trailer()
+
+school_of_rock=ud7_media.Movie("School of Rock","fun story","http://3.bp.blogspot.com/-LIzGt3Rbd_I/USLKZc_VkHI/AAAAAAAADl4/ose8aai1RPM/s1600/Screen+Shot+2013-02-18+at+4.39.37+PM.png","https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+
+movies=[toy_story,avatar,school_of_rock]
+fresh_tomatoes.open_movies_page(movies)
