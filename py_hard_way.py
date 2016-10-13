d241ccc94fe328482cc4cc8088a28535e2bcca2d
Sandesh <vishwanathsandesh@gmail.com>
2016-10-13 16:48:30 +0530

    transpose of a matrix

--- py_hard_way.py
+++ py_hard_way.py
@@ -1,23 +1,14 @@
-def cric_world():
-	print "which one u like btting or bowling"
-	b=raw_input("enter here:")
-	if b=="batting":
-		print "great choice"
-	else:
-		print "good for u"
+class other(object):
+	def implicit(self):
+		print "other implicit"
 		
-def foot_world():
-	print "I don't think we will get along well"
-
-
-def choose_game():
-	print "which one u like 1.cricet 2.football"
-	choice=raw_input("enter ur choice:")
-	if "cricket" in choice:
-		cric_world()
-	elif "football" in choice:
-		foot_world()
-	else:
-		print "i think u didn't understand the choices"
+class child(object):
+	def __init__(self):
+		self.other=other()
 		
-choose_game()
+	def implicit(self):
+		self.other.implicit()
+		
+		
+son=child()
+son.implicit()
---
+++ transpose.py
@@ -0,0 +1,5 @@
+import numpy
+matrix=[[1,2,3],[4,5,6]]
+print matrix
+t_matrix=numpy.transpose(matrix)
+print t_matrix
