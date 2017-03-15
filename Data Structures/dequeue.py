cc3e6f3cda93d3a5865d940a06893d3c01b707f8
Sandesh <vishwanathsandesh@gmail.com>
2017-03-15 17:46:11 +0530

    Dequeue

--- Data Structures/dequeue.py
+++ Data Structures/dequeue.py
@@ -1,14 +1,14 @@
 class Dequeue():
     def __init__(self):
         self.items=[]
-    def addFront(self,item):
-        self.items.insert(0,item)
     def addRear(self,item):
+        self.items.insert(0,item)
+    def addFront(self,item):
         self.items.append(item)
     def removeFront(self):
-        return self.items.pop(0)
-    def removeRear(self):
         return self.items.pop()
+    def removeRear(self):
+        return self.items.pop(0)
     def isEmpty(self):
         if len(self.items)==0:
             return True
